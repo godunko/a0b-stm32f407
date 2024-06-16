eed476c19eb4a8ef1a576ab222425e12432f8759
Vadim Godunko <vgodunko@gmail.com>
2024-06-16 11:58:01 +0400

    Fix Alire crate, project file, and add parent package.

---
+++ source/a0b-stm32f407.ads
@@ -0,0 +1,13 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+package A0B.STM32F407
+  with Preelaborate
+is
+
+end A0B.STM32F407;
