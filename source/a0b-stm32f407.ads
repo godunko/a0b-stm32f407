ec359133ab9639d73ba7ae9e03f0655e44558f11
Vadim Godunko <vgodunko@gmail.com>
2024-07-07 02:14:13 +0400

    DMA_Stream interrupts.

--- source/a0b-stm32f407.ads
+++ source/a0b-stm32f407.ads
@@ -12,24 +12,42 @@ package A0B.STM32F407
   with Preelaborate
 is
 
-   EXTI0     : constant A0B.ARMv7M.External_Interrupt_Number := 6;
-   EXTI1     : constant A0B.ARMv7M.External_Interrupt_Number := 7;
-   EXTI2     : constant A0B.ARMv7M.External_Interrupt_Number := 8;
-   EXTI3     : constant A0B.ARMv7M.External_Interrupt_Number := 9;
-   EXTI4     : constant A0B.ARMv7M.External_Interrupt_Number := 10;
+   EXTI0        : constant A0B.ARMv7M.External_Interrupt_Number := 6;
+   EXTI1        : constant A0B.ARMv7M.External_Interrupt_Number := 7;
+   EXTI2        : constant A0B.ARMv7M.External_Interrupt_Number := 8;
+   EXTI3        : constant A0B.ARMv7M.External_Interrupt_Number := 9;
+   EXTI4        : constant A0B.ARMv7M.External_Interrupt_Number := 10;
+   DMA1_Stream0 : constant A0B.ARMv7M.External_Interrupt_Number := 11;
+   DMA1_Stream1 : constant A0B.ARMv7M.External_Interrupt_Number := 12;
+   DMA1_Stream2 : constant A0B.ARMv7M.External_Interrupt_Number := 13;
+   DMA1_Stream3 : constant A0B.ARMv7M.External_Interrupt_Number := 14;
+   DMA1_Stream4 : constant A0B.ARMv7M.External_Interrupt_Number := 15;
+   DMA1_Stream5 : constant A0B.ARMv7M.External_Interrupt_Number := 16;
+   DMA1_Stream6 : constant A0B.ARMv7M.External_Interrupt_Number := 17;
 
-   EXTI9_5   : constant A0B.ARMv7M.External_Interrupt_Number := 23;
+   EXTI9_5      : constant A0B.ARMv7M.External_Interrupt_Number := 23;
 
-   SPI1      : constant A0B.ARMv7M.External_Interrupt_Number := 35;
-   SPI2      : constant A0B.ARMv7M.External_Interrupt_Number := 36;
+   SPI1         : constant A0B.ARMv7M.External_Interrupt_Number := 35;
+   SPI2         : constant A0B.ARMv7M.External_Interrupt_Number := 36;
 
-   EXTI15_10 : constant A0B.ARMv7M.External_Interrupt_Number := 40;
+   EXTI15_10    : constant A0B.ARMv7M.External_Interrupt_Number := 40;
 
-   SPI3      : constant A0B.ARMv7M.External_Interrupt_Number := 51;
+   DMA1_Stream7 : constant A0B.ARMv7M.External_Interrupt_Number := 47;
+
+   SPI3         : constant A0B.ARMv7M.External_Interrupt_Number := 51;
+
+   DMA2_Stream0 : constant A0B.ARMv7M.External_Interrupt_Number := 56;
+   DMA2_Stream1 : constant A0B.ARMv7M.External_Interrupt_Number := 57;
+   DMA2_Stream2 : constant A0B.ARMv7M.External_Interrupt_Number := 58;
+   DMA2_Stream3 : constant A0B.ARMv7M.External_Interrupt_Number := 59;
+   DMA2_Stream4 : constant A0B.ARMv7M.External_Interrupt_Number := 60;
+
+   DMA2_Stream5 : constant A0B.ARMv7M.External_Interrupt_Number := 68;
+   DMA2_Stream6 : constant A0B.ARMv7M.External_Interrupt_Number := 69;
+   DMA2_Stream7 : constant A0B.ARMv7M.External_Interrupt_Number := 70;
 
    type Function_Line is
-     (
-      SPI1_MISO,
+     (SPI1_MISO,
       SPI1_MOSI,
       SPI1_NSS,
       SPI1_SCK,
@@ -40,7 +58,6 @@ is
       SPI3_MISO,
       SPI3_MOSI,
       SPI3_NSS,
-      SPI3_SCK
-     );
+      SPI3_SCK);
 
 end A0B.STM32F407;
