0a45d1835e7b3bc50f422f967f3c1d76f961be9f
Vadim Godunko <vgodunko@gmail.com>
2024-07-07 02:09:17 +0400

    Interrupts and function lines for EXTI and SPI.

--- source/a0b-stm32f407.ads
+++ source/a0b-stm32f407.ads
@@ -6,8 +6,41 @@
 
 pragma Restrictions (No_Elaboration_Code);
 
+with A0B.ARMv7M;
+
 package A0B.STM32F407
   with Preelaborate
 is
+
+   EXTI0     : constant A0B.ARMv7M.External_Interrupt_Number := 6;
+   EXTI1     : constant A0B.ARMv7M.External_Interrupt_Number := 7;
+   EXTI2     : constant A0B.ARMv7M.External_Interrupt_Number := 8;
+   EXTI3     : constant A0B.ARMv7M.External_Interrupt_Number := 9;
+   EXTI4     : constant A0B.ARMv7M.External_Interrupt_Number := 10;
+
+   EXTI9_5   : constant A0B.ARMv7M.External_Interrupt_Number := 23;
+
+   SPI1      : constant A0B.ARMv7M.External_Interrupt_Number := 35;
+   SPI2      : constant A0B.ARMv7M.External_Interrupt_Number := 36;
+
+   EXTI15_10 : constant A0B.ARMv7M.External_Interrupt_Number := 40;
+
+   SPI3      : constant A0B.ARMv7M.External_Interrupt_Number := 51;
+
+   type Function_Line is
+     (
+      SPI1_MISO,
+      SPI1_MOSI,
+      SPI1_NSS,
+      SPI1_SCK,
+      SPI2_MISO,
+      SPI2_MOSI,
+      SPI2_NSS,
+      SPI2_SCK,
+      SPI3_MISO,
+      SPI3_MOSI,
+      SPI3_NSS,
+      SPI3_SCK
+     );
 
 end A0B.STM32F407;
